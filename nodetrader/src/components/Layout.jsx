f49e38bcaa53a0f1c7fb6d6da855f327938e0043
cilantro <sandro@wustl.edu>
2024-10-20 11:38:04 -0500

    made some changes

--- nodetrader/src/components/Layout.jsx
+++ nodetrader/src/components/Layout.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import Sidebar from './Sidebar';  // Adjust if needed
-import Header from './Header';    // Adjust if needed
+import Sidebar from './Sidebar';  // Adjust path if necessary
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="relative flex min-h-screen">
+      {/* Sidebar */}
+      <Sidebar className="bg-white fixed top-0 left-0 w-16 md:w-56 h-full" />
 
-      <div className="w-full ml-16 md:ml-56">
-        <Header />
-        
+      {/* Main content area */}
+      <div className="flex-1 ml-16 md:ml-56 bg-gray-100 min-h-screen">
         <div className="p-4">
           <Outlet />
         </div>
